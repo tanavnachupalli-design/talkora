2f23024a17588dad5164c964bcb5f50a91b8fdab
tanavnachupalli-design <tanavnachupalli@gmail.com>
2026-02-10 13:50:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-function login() {
-  let name = document.getElementById('name').value;
-  let email = document.getElementById('email').value;
-  let phone = document.getElementById('phone').value;
-  if(name && email && phone) {
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Please enter all details!");
-  }
-}
-
-function book(amount) {
-  localStorage.setItem('amount', amount);
-  window.location.href = "booking.html";
-}
-
-function goToPayment() {
-  let amount = localStorage.getItem('amount');
-  document.getElementById('selected-amount').innerText = amount;
-  window.location.href = "payment.html";
-}
-
-function paymentDone() {
-  window.location.href = "confirmation.html";
-}
+// Utility: Redirect function
+function redirectTo(page) {
+    window.location.href = page;
